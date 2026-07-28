e05aad78a7f0627b17df55dfe9c65312acce65ef
asdfsdf <asdfsdf>
2026-07-28 21:25:36 +0100

    add 3

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Job
+from .models import Category, Job
 
 admin.site.register(Job)
+admin.site.register(Category)
 # Register your models here.
--- job/models.py
+++ job/models.py
@@ -13,7 +13,12 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name  
