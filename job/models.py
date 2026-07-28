7a2b69dae393dd3342152ff4c36bce62ee999c1d
asdfsdf <asdfsdf>
2026-07-28 19:49:30 +0100

    add 2

--- job/models.py
+++ job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django import forms
+choisces = [
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+]
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=20, choices=choisces, default='Full Time')
+    location = models.CharField(max_length=100)
+    published_at = models.DateTimeField()
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
 
 # Create your models here.
